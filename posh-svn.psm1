8dd9b4722ecaef0de3f182902d1736a5b47e2714
Chris Donnelly <chris.donnelly@vauto.com>
2017-07-24 17:34:57 -0500

    more "do not dot source"

--- posh-svn.psm1
+++ posh-svn.psm1
@@ -11,7 +11,26 @@ if ($psv.Major -lt 3 -and !$NoVersionWarn) {
     "To suppress this warning, change your profile to include 'Import-Module posh-svn -Args `$true'.")
 }
 
-& $PSScriptRoot\CheckVersion.ps1 > $null
+# & $PSScriptRoot\CheckVersion.ps1 > $null
+New-TimingInfo -Name CheckVersion -Command {
+    $Global:SvnMissing = $false
+
+    if (!(Get-Command svn -TotalCount 1 -ErrorAction SilentlyContinue)) {
+        Write-Warning "svn command could not be found. Please create an alias or add it to your PATH."
+        $Global:SvnMissing = $true
+        return
+    }
+
+    # HACK determine a minimum required version, 1.6.0 is a guess
+    $requiredVersion = [Version]'1.6.0'
+    if ([String](svn --version 2> $null) -match '(?<ver>\d+(?:\.\d+)+)') {
+        $version = [Version]$Matches['ver']
+    }
+    if ($version -lt $requiredVersion) {
+        Write-Warning "posh-svn requires Subversion $requiredVersion or better. You have $version."
+        return
+    }
+}
 
 @('SvnUtils', 'SvnPrompt', 'SvnTabExpansion') |
     ForEach-Object {
